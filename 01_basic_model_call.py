30d619f47ca98e8abec310a9ecfb3dca3a074f27
sara-hebbadj <sarahebbadj1@gmail.com>
2026-07-23 15:36:35 +0400

    Update 01_basic_model_call.py

--- 01_basic_model_call.py
+++ 01_basic_model_call.py
@@ -7,10 +7,6 @@ model = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
 )
 response = model.invoke("say hello in one short sentence")
-print (response.text)
+print(response.text)
 
-## implement first LangChain Gemini model invocation
-## Load an API key.
-## Connect LangChain to Gemini.
-## Call the model with invoke().
-## Print the response.
+
